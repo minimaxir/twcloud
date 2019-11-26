701c3de314bd9a179f60708338db7d9379d16638
Max Woolf <max@minimaxir.com>
2019-11-25 17:33:47 -0800

    Working tweet retrieval

---
+++ twcloud/twcloud.py
@@ -0,0 +1,32 @@
+import twint
+from stylecloud import gen_stylecloud
+
+
+def get_tweet_text(username=None, search=None, limit=1000):
+    """
+    Returns a list of cleaned tweets.
+    """
+
+    assert any([username, search]), "Either `username` or `search`" \
+        "must be specified"
+    assert limit % 20 == 0, "`limit` must be a multiple of 20."
+
+    c = twint.Config()
+    c.Store_object = True
+
+    # User config
+    c.Search = search
+    c.Username = username
+    c.Limit = limit
+
+    twint.run.Search(c)
+    assert len(twint.output.tweets_list) > 0, "No tweets were returned."
+
+    tweets = [tweet.tweet for tweet in twint.output.tweets_list]
+
+    return tweets
+
+
+if __name__ == "__main__":
+    x = get_tweet_text(username='realdonaldtrump', limit=20)
+    print(x)
