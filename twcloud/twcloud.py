7934fc2631c12a6bd7adfff948e86dc1858a15c9
Max Woolf <max@minimaxir.com>
2019-11-25 17:38:22 -0800

    Terminal formatting

--- twcloud/twcloud.py
+++ twcloud/twcloud.py
@@ -13,12 +13,18 @@ def get_tweet_text(username=None, search=None, limit=1000):
 
     c = twint.Config()
     c.Store_object = True
+    c.Hide_output = True
 
     # User config
     c.Search = search
     c.Username = username
     c.Limit = limit
 
+    # Run the tweet search (may take awhile)
+    if username:
+        print(f"Retrieving {limit} tweets for {username}")
+    else:
+        print(f"Retrieving {limit} tweets about {search}")
     twint.run.Search(c)
     assert len(twint.output.tweets_list) > 0, "No tweets were returned."
 
