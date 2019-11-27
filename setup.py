732341b2cce1e3d658f35535268b5c5f17276fa0
Max Woolf <max@minimaxir.com>
2019-11-26 17:44:07 -0800

    Update setup.py for PyPI

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
 from setuptools import setup, find_packages
 
 long_description = '''
+Generate a word cloud of [Twitter](https://twitter.com/) tweets with a single command.
 
+twcloud is a Python package that leverages the [twint](https://github.com/twintproject/twint) package to gather Tweets from any public account without requiring authentication, and the [stylecloud](https://github.com/minimaxir/stylecloud) package which adds many useful features to create truly unique word clouds!
+
+* Retrieve tweets from a user, or a Twitter search.
+* Gather any number of Tweets from the specified query for building the word cloud (even beyond the typical 3,200 limit)
+* Command Line Interface!
+
+Additionally, from the base `stylecloud` package:
+
+* Icon shapes (of any size!) for word clouds
+* Support for advanced color palettes (via [palettable](https://jiffyclub.github.io/palettable/))
+* Manual color selection for text and backgrounds,
+* Directional gradients w/ the aforementioned palettes.
 '''
 
 
@@ -9,8 +22,8 @@ setup(
     name='twcloud',
     packages=['twcloud'],  # this must be the same as the name above
     version='0.1',
-    description="Python package + CLI to generate wordclouds " \
-    "of Twitter tweets",
+    description="Python package + CLI to generate word clouds " \
+    "of Twitter tweets.",
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Max Woolf',
